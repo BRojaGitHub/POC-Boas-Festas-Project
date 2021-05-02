c59cc1aed2428ef50e1b0f222b9dafd075e017f6
BRojaGitHub <rojabugade@gmail.com>
2021-05-02 15:32:24 +0530

    Initial proj data added

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+     path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('news', views.news, name='news'),
+    path('destinations', views.destinations, name='destinations')
+   
+     ]
